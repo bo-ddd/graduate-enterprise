3df49c7e00080b51154130c061d1072bc0c1146f
lzp885387491 <885387491@qq.com>
2022-11-15 11:53:19 +0800

    jjljgbug

--- src/pages/layout/view/Home.vue
+++ src/pages/layout/view/Home.vue
@@ -6,8 +6,7 @@
                     <div class="left major just-between-2">
                         <div class="flex">
                             <div class="img mr-16">
-                                <img class="icon-head-portrait" :src="EnterpriseInfo.companyLogoUrl"
-                                    :alt="EnterpriseInfo.companyLogoUrl">
+                                <img class="icon-head-portrait" :src="EnterpriseInfo.companyLogoUrl" alt="">
                             </div>
                             <div class="test">
                                 <h1>{{ EnterpriseInfo.companyName }}</h1>
@@ -102,7 +101,9 @@
             <div class="intention mt-16">
                 <div class="just-between">
                     <div class="top">
-                        <h3>意向学校 <span>0</span>/50</h3>
+                        <h3>意向学校
+                            <span>{{ schoolLen }}</span>/50
+                        </h3>
                         <div class="test">
                             <span class="mr-14 mt-17">请尽量选择更多的意向学校</span>
                             <span><img src="@/assets/images/icon-right.png" alt=""></span>
@@ -113,7 +114,7 @@
                     </div>
                 </div>
                 <div class="mt-25 text">
-                    兰州理工大学技术工程学院、甘肃能源化工职业学院、西安科技大学高新学院、成都信息工程大学、华北电力大学、山西工商学院、山西传媒学院、太原学院、太原电力高等专科学校、山西职业技术学院、山西轻工职业技术学院、忻州师范学院、运城职业技术学院、长治职业技术学院、西安电子科技大学、渭南职业技术学院
+                    {{ change }}
                 </div>
             </div>
 
@@ -165,7 +166,7 @@
             <div class="dialog_body">
                 <div class="dialog_body-header-r">
                     <div class="dialog_body-header-r_test">
-                        <b class="c356ffb"> {{ selectValue.length }} </b>
+                        <b class="c356ffb"> {{ EnterpriseInfo.companyWishSchool.length }} </b>
                         <span>/50</span>
                     </div>
                 </div>
@@ -173,7 +174,7 @@
                     <label class="mr-12">意向学校</label>
                     <el-select v-model="selectValue" size="large" multiple filterable allow-create default-first-option
                         :reserve-keyword="false" placeholder="请输入">
-                        <el-option v-for="item in options" :key="item.schoolId" :label="item.schoolName"
+                        <el-option v-for="item in schoolList" :key="item.schoolId" :label="item.schoolName"
                             :value="item.schoolId" />
                     </el-select>
                 </div>
@@ -206,22 +207,26 @@
 
 <script setup lang="ts">
 import { useHomeStore } from '@/stores/home';
-import { ref } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import type { Ref } from "vue";
 import footerBar from '@/components/footer/footerBar.vue';
+// import { useRouter } from 'vue-router';
 import { ElMessage } from 'element-plus'
+// 路由
+// let router = useRouter();
 // ajax
 const use = useHomeStore();
 // 跳转页面的方法
-const nav = (name: string) => {
-    window.location.href = name + '.html';
+let nav = (name: string) => {
+    window.location.href = `${name}.html`
 }
 // 控制意向学校弹层的开关
 const centerDialogVisible = ref(false)
 // 意向学校 多选框选择的值
 let selectValue = ref([]);
+const schoolLen = computed(() => EnterpriseInfo.companyWishSchool.length);
 // 意向学校 数据
-const options: Ref<Array<{
+const schoolList: Ref<Array<{
     schoolId: Number,
     sortId: Number,
     schoolName: string,
@@ -229,7 +234,8 @@ const options: Ref<Array<{
 // 获取意向学校的数据
 let getSchoolList = async function () {
     let res = await use.getSchoolList()
-    Object.assign(options.value, res.data)
+    Object.assign(schoolList.value, res.data)
+    console.log('获取意向学校的数据', schoolList.value);
 }
 getSchoolList();
 // 单选框组的值
@@ -238,20 +244,20 @@ const radioValue = ref(false)
 const confirm = () => {
     console.log('选中的意向学校的 ID：', selectValue.value)
     console.log('单选框选择的是：', radioValue.value, radioValue.value == true ? '仅意向' : '所有学校')
-    let options: any = [];
+    let companyWishSchool: any = [];
     selectValue.value.forEach(element => {
-        options.push(element);
+        companyWishSchool.push(element);
     });
     // 点击提交按钮的时候用  调接口传参
     setEnterpriseSchoolOfIntention({
         companyOnlyWishSchool: radioValue.value,
-        companyWishSchool: JSON.stringify(options),
+        companyWishSchool,
         userId: 10000,
     })
 }
 interface setEnterpriseSchoolOfIntentionType {
     companyOnlyWishSchool: boolean,// 企业仅向意向学校展示职位 
-    companyWishSchool: string,// 企业意向学校 多选格式1,2,3 ,
+    companyWishSchool: [Number] | any,// 企业意向学校 多选格式1,2,3 ,
     userId: Number,// 用户id
 }
 // 这是修改意向学校的接口
@@ -287,7 +293,8 @@ interface EnterpriseInfoType {
     companyStatus: string,// 企业状态
     companyTag: string,// 企业标签
     companyWebUrl: string,// 企业官网
-    companyWishSchool: string | any,// 企业意向学校 ,
+    companyWishSchool: number[] | [],// 企业意向学校 ,
+    companyWishSchool1: string,// 企业意向学校 ,
     invitationTalentCount: Number,// 邀请人才点数 ,
     newResumeCount: Number,// 日内未查看简历数量 ,
     onlinePositionCount: Number,// 在招职位数量 ,
@@ -298,14 +305,30 @@ interface EnterpriseInfoType {
     smsInvitationCount: Number,// 短信邀请人才点数
     userId: Number,// 用户ID
 }
-const EnterpriseInfo: EnterpriseInfoType | any = ref([]);
+const EnterpriseInfo: EnterpriseInfoType = reactive({
+    companyWishSchool: []
+}) as EnterpriseInfoType;
 // 获取企业详细信息接口
+const change = ref<string>('')
 let getEnterpriseInfo = async () => {
     let res = await use.getEnterprise({ userId: 10000 });
-    Object.assign(EnterpriseInfo.value, res.data)
-    console.log(EnterpriseInfo.value);
+    Object.assign(EnterpriseInfo, res.data)
+    console.log('获取企业详细信息接口', EnterpriseInfo);
+    change.value = ''
+    schoolList.value.forEach((el: any) => {
+        EnterpriseInfo.companyWishSchool.forEach((element: Number) => {
+            if (el.schoolId == element) {
+                change.value += el.schoolName + '、'
+                console.log(change.value)
+            }
+        });
+    });
+    if (change.value == '' || change.value == undefined) {
+        getEnterpriseInfo()
+    }
 }
 getEnterpriseInfo()
+
 </script>
 
 <style lang="scss" scoped>
