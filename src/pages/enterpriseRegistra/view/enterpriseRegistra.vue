968e917ed489dab2ae24006d0869bb91ac6f8dc2
lzp885387491 <885387491@qq.com>
2022-12-07 20:07:05 +0800

    Update code

--- src/pages/enterpriseRegistra/view/enterpriseRegistra.vue
+++ src/pages/enterpriseRegistra/view/enterpriseRegistra.vue
@@ -9,7 +9,8 @@
                     <h3>基本信息</h3>
                     <!-- 企业全称 -->
                     <el-form-item label="企业全称">
-                        <el-input disabled class="el-input_560-40" placeholder="需与营业执照一致" v-model="form.companyFullName" />
+                        <el-input disabled class="el-input_560-40" placeholder="需与营业执照一致"
+                            v-model="form.companyFullName" />
                     </el-form-item>
 
                     <!-- 品牌全称 -->
@@ -381,7 +382,9 @@ const onSubmit = async () => {
             type: 'success',
         })
         getEnterprise();
-        window.location.href = '/';
+        setTimeout(() => {
+            window.location.href = '/';
+        }, 1000);
     };
 };
 </script>
