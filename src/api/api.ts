b88dca86922829b9c254b7f3b72c33d9b29d7b40
yang <1145938287@qq.com>
2023-01-02 21:28:04 +0800

    news

--- src/api/api.ts
+++ src/api/api.ts
@@ -297,8 +297,11 @@ export default {
     testsms: () => {
         return axios.get('/testsms', getConfig)
     },
-    // 自动刷新
-    autoRefreshPosition: () => {
-        return axios.get('/company/autoRefreshPosition', getConfig)
+
+    /**
+     * @description 自动刷新
+     */
+    autoRefreshPosition: (params: any) => {
+        return axios.post('/company/autoRefreshPosition',params, postConfig)
     }
 }
